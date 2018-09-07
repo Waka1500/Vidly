e3177f777e0ab663b3e9d9ac3a9a34e29f7322db
Karl <karlwall95@hotmail.com>
2018-09-07 16:30:22 +0200

    Section 3, Exercise 3

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,37 +5,42 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
         //Movies
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(M => M.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(M => M.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
             return View(movie);
-        }
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" },
-                new Movie { Id = 2, Name = "The Lion King" }
-            };
         }
 
     }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        public Genre Genre { get; set; }
+        public byte NumberInStock { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
     }
 }
